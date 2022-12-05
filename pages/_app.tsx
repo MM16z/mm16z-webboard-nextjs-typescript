88a092433c367d522cc322096c61c6d987f572ae
MM16z <imm0rz55160@gmail.com>
2022-12-05 09:13:51 +0700

    .#2 Final deploy #connected to backend server

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,13 +16,28 @@ import Hambergermenu from "../components/hambergermenu";
 import Mobilemenu from "../components/mobilemenu";
 import { createContext, useRef } from "react";
 
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
 import type { AppProps } from "next/app";
+import Router from "next/router";
 
 export const Appcontext = createContext({} as any);
 
 function MyApp({ Component, pageProps }: AppProps) {
   const mobilemenuref = useRef<HTMLDivElement>(null);
   const hambergermenuref = useRef<SVGSVGElement>(null);
+
+  NProgress.configure({
+    minimum: 0.3,
+    easing: "ease",
+    speed: 800,
+    showSpinner: true,
+  });
+
+  Router.events.on("routeChangeStart", () => NProgress.start());
+  Router.events.on("routeChangeComplete", () => NProgress.done());
+  Router.events.on("routeChangeError", () => NProgress.done());
 
   return (
     <Appcontext.Provider value={hambergermenuref}>
