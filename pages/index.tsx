f839ac57ceacb0f99ac9a06a810499dfb4049128
Min16z <min@finema.co>
2023-07-06 01:35:51 +0700

    [fix] - update const name

--- pages/index.tsx
+++ pages/index.tsx
@@ -37,13 +37,13 @@ function Home({ posts }: PostDataType) {
 
   const router = useRouter();
 
-  const useAuth = useAuthStore((state) => state.accessToken);
-  const useUserId = useAuthStore((state) => state.userId);
-  const useUserName = useAuthStore((state) => state.userName);
+  const useAuth = useAuthStore((state: any) => state.accessToken);
+  const useUserId = useAuthStore((state: any) => state.userId);
+  const useUserName = useAuthStore((state: any) => state.userName);
 
-  const setAuthStore = useAuthStore((state) => state.setAccessToken);
-  const setUserId = useAuthStore((state) => state.setUserId);
-  const setUserName = useAuthStore((state) => state.setUserName);
+  const setAuthStore = useAuthStore((state: any) => state.setAccessToken);
+  const setUserId = useAuthStore((state: any) => state.setUserId);
+  const setUserName = useAuthStore((state: any) => state.setUserName);
 
   const [isLoading, setIsLoading] = useState(true);
 
@@ -273,9 +273,7 @@ function Home({ posts }: PostDataType) {
                   key={index}
                   postLikedCount={postLikedCounts[index]}
                   // postLikedCount={postLikedCount}
-                  defaultChecked={
-                    post.isLiked === null ? false : post.isLiked ? true : false
-                  }
+                  defaultChecked={post.isliked}
                   onChange={(e: ChangeEvent<HTMLInputElement>) => {
                     onPostlikeHandler(e, postId, index);
                   }}
