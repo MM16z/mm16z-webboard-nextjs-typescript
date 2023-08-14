0028de74a4e4819a7859d759ebb65f7ffd114f96
Min16z <min@finema.co>
2023-08-15 06:36:11 +0700

    [fix] - logout bug

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,94 +1,108 @@
 import Link from "next/link";
-import { useRouter } from "next/router";
-import { useRef } from "react";
+import {useRouter} from "next/router";
+import {useRef} from "react";
 
 import Cookies from "js-cookie";
 
 import useAuthStore from "../global_state/authStore";
 import axios from "axios";
+import swal from "sweetalert2";
 
 function Navbar() {
-  const router = useRouter();
-  const createPostBtnref = useRef(null);
+    const router = useRouter();
+    const createPostBtnref = useRef(null);
 
-  const useAuth = useAuthStore((state:any) => state.accessToken);
-  const setAuthStore = useAuthStore((state:any) => state.setAccessToken);
+    const useAuth = useAuthStore((state: any) => state.accessToken);
+    const setAuthStore = useAuthStore((state: any) => state.setAccessToken);
 
-  const logoutHandler = () => {
-    axios.post(
-        `${process.env.NEXT_PUBLIC_API_URL}/logout`,
-      JSON.stringify({}),
-      {
-        headers: {
-          "Content-Type": "application/json",
-        },
-        withCredentials: true,
-      }
-    );
-    setAuthStore(null);
-    Cookies.set("u_id", "");
-    //call logout api to delete cookie
-    window.location.href = "/";
-  };
-
-  return (
-    <nav className="navbar-container">
-      <div
-        className="navbar-title"
-        onClick={() => {
-          router.push("/");
-        }}
-      >
-        MM16STUDIO
-      </div>
-      <div
-        className="navbar-title_01"
-        onClick={() => {
-          router.push("/");
-        }}
-      >
-        Webboard
-      </div>
-      {useAuth ? null : (
-        <Link href="/login" className={"navbar-text-login"}>
-          Login
-        </Link>
-      )}
-      {useAuth ? null : (
-        <Link href="/register" className={"navbar-text-register"}>
-          Register |
-        </Link>
-      )}
-      {router.pathname === "/userpanel" ? null : (
-        <div
-          ref={createPostBtnref}
-          className={
-            useAuth
-              ? "navbar-text-createpost_isLogin"
-              : "navbar-text-createpost"
-          }
-          onClick={() => {
-            if (useAuth) {
-              router.push("userpanel");
-            } else {
-              router.push("login");
+    const logoutHandler = () => {
+        axios.post(
+            `${process.env.NEXT_PUBLIC_API_URL}/logout`,
+            JSON.stringify({}),
+            {
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                withCredentials: true,
             }
-          }}
-          style={{ right: useAuth ? "140px" : "295px" }}
-        >
-          CreatePost |
-        </div>
-      )}
-      {useAuth ? (
-        <div className="navbar-text-logout" onClick={logoutHandler}>
-          Logout
-        </div>
-      ) : null}
-      <span className="navbar-bg">
+        ).then((res) => {
+            if (res.status !== 204) {
+                return swal.fire({
+                    icon: 'error',
+                    title: 'xdding?',
+                    text: `Logout failed!`,
+                })
+            }
+            swal.fire({
+                icon: 'success',
+                title: 'xdding?',
+                text: `Logout success!`,
+            })
+            setAuthStore(null);
+            Cookies.set("u_id", "");
+            window.location.href = "/";
+        });
+        //call logout api to delete cookie
+    };
+
+    return (
+        <nav className="navbar-container">
+            <div
+                className="navbar-title"
+                onClick={() => {
+                    router.push("/");
+                }}
+            >
+                MM16STUDIO
+            </div>
+            <div
+                className="navbar-title_01"
+                onClick={() => {
+                    router.push("/");
+                }}
+            >
+                Webboard
+            </div>
+            {useAuth ? null : (
+                <Link href="/login" className={"navbar-text-login"}>
+                    Login
+                </Link>
+            )}
+            {useAuth ? null : (
+                <Link href="/register" className={"navbar-text-register"}>
+                    Register |
+                </Link>
+            )}
+            {router.pathname === "/userpanel" ? null : (
+                <div
+                    ref={createPostBtnref}
+                    className={
+                        useAuth
+                            ? "navbar-text-createpost_isLogin"
+                            : "navbar-text-createpost"
+                    }
+                    onClick={() => {
+                        if (useAuth) {
+                            router.push("userpanel");
+                        } else {
+                            router.push("login");
+                        }
+                    }}
+                    style={{right: useAuth ? "140px" : "295px"}}
+                >
+                    CreatePost |
+                </div>
+            )}
+            {useAuth ? (
+                <div className="navbar-text-logout" onClick={logoutHandler}>
+                    Logout
+                </div>
+            ) : null}
+            <span className="navbar-bg">
         <span className="navbar-bg-nested"></span>
       </span>
-    </nav>
-  );
+        </nav>
+    );
 }
 
 export default Navbar;
