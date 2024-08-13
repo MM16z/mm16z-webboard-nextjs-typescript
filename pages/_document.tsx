135ec79b167001a677e6ef95dfe5b65b0e75ee26
MM16z <76926089+MM16z@users.noreply.github.com>
2024-08-13 10:09:47 +0700

    [fix] - cors

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html>
       <Head />
-      <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <title>MM16z | Webboard</title>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
